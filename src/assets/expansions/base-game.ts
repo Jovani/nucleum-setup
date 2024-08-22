14ff4d67ac5a384d49cbfdf2261f63c01c4a2501
Ivan Djokic <jovani.ivan@gmail.com>
2024-08-21 20:41:05 -0400

    Base Game fixture

--- src/assets/expansions/base-game.ts
+++ src/assets/expansions/base-game.ts
@@ -1,3 +1,4 @@
+import { Cities } from './base-game.enum';
 import { Expansion } from './card-sets.interface';
 
 export const baseGame: Expansion = {
@@ -23,79 +24,295 @@ export const baseGame: Expansion = {
     },
     cards: [
         {
-            key: 'AUS10',
-            publicBuilding: 'Wagga Wagga',
-            nucleumToken: 'Toowoomba',
-            noBuildings: [
-                'Port Pirie',
-                'Sydney',
-                'Newcastle',
-                'Adelaide',
-            ],
+            key: 'S1',
+            publicBuilding: Cities.Leipzig,
+            nucleumToken: Cities.Grimma,
+            noBuildings: {
+                2: [
+                    Cities.Grimma,
+                    Cities.Dresden,
+                    Cities.Praha,
+                    Cities.Praha
+                ],
+                3: [
+                    Cities.Gorlitz,
+                ]
+            },
             noMines: {
                 2: [
-                    'Radium Hill',
-                    'Tonngi',
-                    'Latrobe',
+                    Cities.Presnitz,
+                    Cities.Joachimsthal,
                 ],
-                3: []
+                3: [
+                    Cities.Karlsbad,
+                ]
             }
         },
         {
-            key: 'AUS11',
-            publicBuilding: 'Port Pirie',
-            nucleumToken: 'Lucas Heights',
-            noBuildings: [
-                'Latrobe',
-                'Sydney',
-                'Brisbane',
-                'Brisbane',
-            ],
+            key: 'S2',
+            publicBuilding: Cities.Leipzig,
+            nucleumToken: Cities.Zittau,
+            noBuildings: {
+                2: [
+                    Cities.Marienberg,
+                    Cities.Bautzen,
+                    Cities.Leipzig,
+                    Cities.Praha,
+                ]
+            },
             noMines: {
                 2: [
-                    'Mount Thorley',
-                    'Mount Thorley',
-                    'Radium Hill'
-                ],
-                3: []
+                    Cities.Aussig,
+                    Cities.Joachimsthal,
+                    Cities.Brux,
+                ]
             }
         },
         {
-            key: 'AUS12',
-            publicBuilding: 'Newcastle',
-            nucleumToken: 'Mildura',
-            noBuildings: [
-                'Latrobe',
-                'Geolong',
-                'Wollongong',
-                'Wagga Wagga',
-            ],
-            noMines: {
-            2: [
-                'Radium Hill',
-                'Toongi (Coal)',
-            ],
-            3: [
-                'Wooltana'
-            ]}
-        },
-        {
-            key: 'AUS13',
-            publicBuilding: '',
-            nucleumToken: 'Geelong',
-            noBuildings: [
-                'Gold Coast',
-                'Melbourne',
-                'Hobart',
-                'Newcastle',
-            ],
+            key: 'S3',
+            publicBuilding: Cities.Grimma,
+            nucleumToken: Cities.Zittau,
+            noBuildings: {
+                2: [
+                    Cities.Dresden,
+                    Cities.Dresden,
+                    Cities.Leipzig,
+                    Cities.Leipzig,
+                    Cities.Zwickau,
+                ]
+            },
             noMines: {
                 2: [
-                    'Radium Hill',
-                    'Toongi (Coal)',
-                    'Olympic Dam'
+                    Cities.Presnitz,
+                    Cities.Aussig,
+                ]
+            }
+        },
+        {
+            key: 'S4',
+            publicBuilding: Cities.Chemnitz,
+            nucleumToken: Cities.Glashutte,
+            noBuildings: {
+                2: [
+                    Cities.Grimma,
+                    Cities.Riesa,
+                    Cities.Plauen,
+                    Cities.Freiberg,
                 ],
-                3: []
+                3: [
+                    Cities.Gorlitz,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Brux,
+                    Cities.Presnitz,
+                ]
+            }
+        },
+        {
+            key: 'S5',
+            publicBuilding: Cities.Freiberg,
+            nucleumToken: Cities.Zittau,
+            noBuildings: {
+                2: [
+                    Cities.Riesa,
+                    Cities.Freiberg,
+                    Cities.Chemnitz,
+                ],
+                3: [
+                    Cities.Chemnitz,
+                    Cities.Karlsbad,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Brux,
+                    Cities.Aussig,
+                ]
+            }
+        },
+        {
+            key: 'S6',
+            publicBuilding: Cities.Dresden,
+            nucleumToken: Cities.Plauen,
+            noBuildings: {
+                2: [
+                    Cities.Marienberg,
+                    Cities.Bautzen,
+                    Cities.Grimma,
+                    Cities.Riesa,
+                    Cities.Zwickau,
+                ],
+                3: [
+                    Cities.Gorlitz,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Chemnitz,
+                    Cities.Joachimsthal,
+                ],
+                3: [
+                    Cities.Karlsbad,
+                ]
+            }
+        },
+        {
+            key: 'S7',
+            publicBuilding: Cities.Bautzen,
+            nucleumToken: Cities.Plauen,
+            noBuildings: {
+                2: [
+                    Cities.Dresden,
+                    Cities.Grimma,
+                ],
+                3: [
+                    Cities.Karlsbad,
+                    Cities.Gorlitz,
+                    Cities.Chemnitz,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Aussig,
+                    Cities.Presnitz,
+                ],
+                3: [
+                    Cities.Brux,
+                ]
+            }
+        },
+        {
+            key: 'S8',
+            publicBuilding: Cities.Dresden,
+            nucleumToken: Cities.Glashutte,
+            noBuildings: {
+                2: [
+                    Cities.Marienberg,
+                    Cities.Praha,
+                    Cities.Praha,
+                    Cities.Leipzig,
+                    Cities.Leipzig,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Brux,
+                    Cities.Joachimsthal,
+                ],
+                3: [
+                    Cities.Karlsbad,
+                ]
+            }
+        },
+        {
+            key: 'S9',
+            publicBuilding: Cities.Marienberg,
+            nucleumToken: Cities.Grimma,
+            noBuildings: {
+                2: [
+                    Cities.Bautzen,
+                    Cities.Plauen,
+                    Cities.Praha,
+                    Cities.Freiberg,
+                ],
+                3: [
+                    Cities.Gorlitz,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Aussig,
+                    Cities.Joachimsthal,
+                ],
+                3: [
+                    Cities.Karlsbad,
+                ]
+            }
+        },
+        {
+            key: 'S10',
+            publicBuilding: Cities.Zwickau,
+            nucleumToken: Cities.Plauen,
+            noBuildings: {
+                2: [
+                    Cities.Leipzig,
+                    Cities.Riesa,
+                    Cities.Praha,
+                    Cities.Zwickau,
+                    Cities.Grimma,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Aussig,
+                    Cities.Joachimsthal,
+                    Cities.Presnitz,
+                ]
+            }
+        },
+        {
+            key: 'S11',
+            publicBuilding3Plus: Cities.Karlsbad,
+            nucleumToken: Cities.Zittau,
+            noBuildings: {
+                2: [
+                    Cities.Leipzig,
+                    Cities.Leipzig,
+                    Cities.Dresden,
+                    Cities.Dresden,
+                    Cities.Marienberg,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Aussig,
+                    Cities.Presnitz,
+                    Cities.Brux,
+                ]
+            }
+        },
+        {
+            key: 'S12',
+            publicBuilding: Cities.Praha,
+            nucleumToken: Cities.Glashutte,
+            noBuildings: {
+                2: [
+                    Cities.Marienberg,
+                    Cities.Chemnitz,
+                    Cities.Bautzen,
+                    Cities.Riesa,
+                ],
+                3: [
+                    Cities.Karlsbad,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Aussig,
+                    Cities.Presnitz,
+                    Cities.Joachimsthal,
+                ]
+            }
+        },
+        {
+            key: 'S13',
+            nucleumToken: Cities.Grimma,
+            noBuildings: {
+                2: [
+                    Cities.Dresden,
+                    Cities.Plauen,
+                    Cities.Chemnitz,
+                    Cities.Freiberg,
+                    Cities.Grimma,
+                ]
+            },
+            noMines: {
+                2: [
+                    Cities.Aussig,
+                    Cities.Presnitz,
+                ]
             }
         }
     ]
